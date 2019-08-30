3eb1f426a725b02e79a165c0b9ad8b8f607eb8e3
Mauricio Iriarte Valenzuela <mauricio.iriarte@jalasoft.org>
2019-08-30 13:22:06 -0400

    Adding class ChessQuare

--- src/main/java/jalasoft/bootcamp/chessgame/Piece.java
+++ src/main/java/jalasoft/bootcamp/chessgame/Piece.java
@@ -1,14 +1,45 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package jalasoft.bootcamp.chessgame;
+public abstract class Piece {
+    private String type;
+    private String color;
+    private char rowCurrent;
+    private byte columnCurrent;
 
-/**
- *
- * @author BootCamp LP
- */
-public class Piece {
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public char getRowCurrent() {
+        return rowCurrent;
+    }
+
+    public void setRowCurrent(char rowCurrent) {
+        this.rowCurrent = rowCurrent;
+    }
+
+    public byte getColumnCurrent() {
+        return columnCurrent;
+    }
+
+    public void setColumnCurrent(byte columnCurrent) {
+        this.columnCurrent = columnCurrent;
+    }
+    
+   public boolean move()
+   {
+       return true;
+   }
     
 }
